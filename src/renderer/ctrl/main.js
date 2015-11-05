e4318530c222a6b8b0f4f6f0225af5f1bfeecbaf
Risejandi Autida <risejandiautida@gmail.com>
2015-11-05 11:58:22 +0800

    added financers

--- src/renderer/ctrl/main.js
+++ src/renderer/ctrl/main.js
@@ -4,8 +4,29 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-function MainCtrl ($scope, Database) {
-  Database.sync({logging: console.log});
-};
+var Class = require("../constituent.min.js").Class;
 
-exports.MainCtrl = MainCtrl;
+exports.MainCtrl = Class(function($scope) {
+  $scope.main = this;
+
+  this.financers = [
+    {
+      id: 1,
+      name: "Financer 1"
+    },
+    {
+      id: 2,
+      name: "Financer 2"
+    },
+    {
+      id: 3,
+      name: "Financer 3"
+    }
+  ];
+
+  this.selected = this.financers[0];
+})
+.method("select", function(financer) {
+  console.log(financer);
+  this.selected = financer;
+})
