66488b93553c9ea82df2a61cfbb1625919190c48
Risejandi Autida <risejandiautida@gmail.com>
2015-11-23 11:51:53 +0800

    adjusted column widths

--- src/renderer/svc/column-defs.js
+++ src/renderer/svc/column-defs.js
@@ -9,22 +9,22 @@ var Class = require('../constituent.min.js').Class;
 exports.ColumnDefs = Class(function () {})
 .method("main", function () {
   return [
-    {name: "Name", width: 100, pinnedLeft: true},
-    {name: "Date Taken", width: 100, pinnedLeft: true},
-    {name: "Principal", width: 100},
-    {name: "Interest", width: 100},
-    {name: "Jan", width: 50},
-    {name: "Feb", width: 50},
-    {name: "Mar", width: 50},
-    {name: "Apr", width: 50},
-    {name: "May", width: 50},
-    {name: "Jun", width: 50},
-    {name: "Jul", width: 50},
-    {name: "Aug", width: 50},
-    {name: "Sep", width: 50},
-    {name: "Oct", width: 50},
-    {name: "Nov", width: 50},
-    {name: "Dec", width: 50}
+    {name: "Name", width: 150, pinnedLeft: true},
+    {name: "Date Taken", width: 150, pinnedLeft: true},
+    {name: "Principal", width: 150},
+    {name: "Interest", width: 150},
+    {name: "Jan", width: 75},
+    {name: "Feb", width: 75},
+    {name: "Mar", width: 75},
+    {name: "Apr", width: 75},
+    {name: "May", width: 75},
+    {name: "Jun", width: 75},
+    {name: "Jul", width: 75},
+    {name: "Aug", width: 75},
+    {name: "Sep", width: 75},
+    {name: "Oct", width: 75},
+    {name: "Nov", width: 75},
+    {name: "Dec", width: 75}
   ];
 })
 .method("collections", function () {
